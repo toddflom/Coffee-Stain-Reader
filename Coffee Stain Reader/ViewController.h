646b73e44c2569467cf29219e3ef17418612cd15
Todd Flom <toddf@flom.com>
2013-06-14 12:59:41 -0500

    6-14 1:00pm

--- Coffee Stain Reader/ViewController.h
+++ Coffee Stain Reader/ViewController.h
@@ -8,6 +8,23 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController <UIWebViewDelegate> {
+    UIWebView *mWebView;
+    UIToolbar *mToolbar;
+    UIBarButtonItem *mBack;
+    UIBarButtonItem *mStop;
+    UIBarButtonItem *mRefresh;
+    UIBarButtonItem *mForward;
+}
+
+
+@property (strong, nonatomic) IBOutlet UIWebView *webView;
+@property (strong, nonatomic) IBOutlet UIToolbar *toolbar;
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *back;
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *stop;
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *refresh;
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *forward;
+
+
 
 @end
--- Coffee Stain Reader/ViewController.m
+++ Coffee Stain Reader/ViewController.m
@@ -10,17 +10,98 @@
 
 @interface ViewController () {
     
-    UIButton *tempButt;
+
 }
 
 @end
 
 @implementation ViewController
 
-- (void)viewDidLoad
+
+@synthesize webView = mWebView;
+@synthesize toolbar = mToolbar;
+@synthesize back = mBack;
+@synthesize stop = mStop;
+@synthesize refresh = mRefresh;
+@synthesize forward = mForward;
+
+
+
+- (void) viewDidLoad
 {
     [super viewDidLoad];
+    NSAssert(self.back, @"Unconnected IBOutlet 'back'");
+    NSAssert(self.forward, @"Unconnected IBOutlet 'forward'");
+    NSAssert(self.refresh, @"Unconnected IBOutlet 'refresh'");
+    NSAssert(self.stop, @"Unconnected IBOutlet 'stop'");
+    NSAssert(self.webView, @"Unconnected IBOutlet 'webView'");
+    
+    self.webView.delegate = self;
+    self.webView.scalesPageToFit = YES;
+    NSURL *url = [NSURL URLWithString:@"http://iosdeveloperzone.com"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    [self.webView loadRequest:request];
+    [self updateButtons];
+}
+
+
+
+
+
+#pragma mark -
+#pragma mark UIWebViewDelegate protocol
+
+- (BOOL) webView:(UIWebView *)webView shouldStartLoadWithRequest:(NSURLRequest *)request navigationType:(UIWebViewNavigationType)navigationType
+{
+    return YES;
+}
+
+- (void) webViewDidStartLoad:(UIWebView *)webView
+{
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+    [self updateButtons];
+}
+
+- (void) webViewDidFinishLoad:(UIWebView *)webView
+{
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    [self updateButtons];
+}
+
+- (void) webView:(UIWebView *)webView didFailLoadWithError:(NSError *)error
+{
+    [UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+    [self updateButtons];
+}
+
+
+- (void) updateButtons
+{
+    self.forward.enabled = self.webView.canGoForward;
+    self.back.enabled = self.webView.canGoBack;
+    self.stop.enabled = self.webView.loading;
+}
+
+
+
+
+
+
+
+
+
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.webView = nil;
+    self.toolbar = nil;
+    self.back = nil;
+    self.forward = nil;
+    self.refresh = nil;
+    self.stop = nil;
 }
 
 - (void)didReceiveMemoryWarning
