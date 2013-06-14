ce6c19c9eca4d5760bdfd203832947c38febf5ef
Todd Flom <toddf@flom.com>
2013-06-14 10:44:32 -0500

    second commit

---
+++ Coffee Stain Reader/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Coffee Stain Reader
+//
+//  Created by Todd Flom on 6/14/13.
+//  Copyright (c) 2013 Carmichael Lynch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Coffee Stain Reader/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Coffee Stain Reader
+//
+//  Created by Todd Flom on 6/14/13.
+//  Copyright (c) 2013 Carmichael Lynch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
